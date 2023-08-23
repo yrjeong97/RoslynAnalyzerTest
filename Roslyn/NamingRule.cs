c75238931087a23aa2bcfa4ac77b0ed606e0cf19
yrjeong97 <yrjeong@ati2000.co.kr>
2023-08-23 16:25:25 +0900

    dddd

--- Roslyn/NamingRule.cs
+++ Roslyn/NamingRule.cs
@@ -53,7 +53,7 @@ namespace Roslyn
             return !string.IsNullOrEmpty(s) && char.IsUpper(s[0]);
         }
 
-        void yyyyy()
+        void yyyy()
         {
             // 테스트입니다. josephine 0823
         }
