47126eb79eee3f26fbe2091cca6d4f6828f1b776
yrjeong97 <yrjeong@ati2000.co.kr>
2023-08-23 14:53:21 +0900

    roslyn back

--- Roslyn/Program.cs
+++ Roslyn/Program.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System.Diagnostics;
 
 class Program
 {
@@ -53,71 +52,16 @@ class Program
         if (nonPascalNames.Any())
         {
             var reportContent = string.Join(Environment.NewLine, nonPascalNames);
-            var reportFilePath = "non_pascal_names00.txt";
+            var reportFilePath = "non_pascal_names.txt";
 
             File.WriteAllText(reportFilePath, reportContent);
             Console.WriteLine("Non-PascalCase names report saved.");
-
-            RunGitCommands(csFiles, reportFilePath); //추가된 부분
         }
         else
         {
             Console.WriteLine("All names follow PascalCase.");
         }
-
-        
     }
-    static void RunGitCommands(string[] csFiles, string reportFilePath)
-    {
-        // Git 작업을 위한 커맨드 실행
-        var gitAddCommand = $"git add {reportFilePath}";
-        var gitCommitCommand = $"git commit -m \"Add non-PascalCase names report\"";
-        var gitPushCommand = "git push origin main";
-
-        foreach (var csFile in csFiles)
-        {
-            // Git 작업 수행
-            RunShellCommand(gitAddCommand, csFile);
-            RunShellCommand(gitCommitCommand, csFile);
-            RunShellCommand(gitPushCommand, csFile);
-        }
-    }
-
-    static void RunShellCommand(string command, string workingDirectory)
-    {
-        var processInfo = new ProcessStartInfo("bash", $"-c \"{command}\"")
-        {
-            WorkingDirectory = workingDirectory,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
-
-        var process = new Process
-        {
-            StartInfo = processInfo
-        };
-
-        process.Start();
-        process.WaitForExit();
-
-        var output = process.StandardOutput.ReadToEnd();
-        var error = process.StandardError.ReadToEnd();
-
-        process.Dispose();
-
-        if (!string.IsNullOrEmpty(output))
-        {
-            Console.WriteLine(output);
-        }
-
-        if (!string.IsNullOrEmpty(error))
-        {
-            Console.WriteLine(error);
-        }
-    }
-
 
     static bool IsPascalCase(string s)
     {
