68e91d78a246fbc2f724f72c3e95e176d6fe678a
yrjeong97 <yrjeong@ati2000.co.kr>
2023-08-24 10:54:40 +0900

    update workflow
    변경된 파일만

--- Roslyn/NamingRule.cs
+++ Roslyn/NamingRule.cs
@@ -53,7 +53,7 @@ namespace Roslyn
             return !string.IsNullOrEmpty(s) && char.IsUpper(s[0]);
         }
 
-        void adfadf()
+        void afdsadfdsf()
         {
             // 테스트입니다. josephine 0823
         }
