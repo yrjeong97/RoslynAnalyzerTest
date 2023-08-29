a2af762f574125bd9d085c2f82174eb5b033a98a
yrjeong97 <yrjeong@ati2000.co.kr>
2023-08-29 15:47:43 +0900

    update list changed files

--- Roslyn/NamingRule.cs
+++ Roslyn/NamingRule.cs
@@ -53,7 +53,7 @@ namespace Roslyn
             return !string.IsNullOrEmpty(s) && char.IsUpper(s[0]);
         }
 
-        void dd()
+        void ee()
         {
             // 테스트입니다. josephine 0823
         }
