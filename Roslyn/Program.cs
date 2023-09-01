13774aa35a2f238cf633cae976e38e5512854a4e
yrjeong97 <yrjeong@ati2000.co.kr>
2023-09-01 17:28:57 +0900

    Update Program.cs

--- Roslyn/Program.cs
+++ Roslyn/Program.cs
@@ -24,57 +24,57 @@ class Program
 
         var csFilesList = changedFiles.Split(';'); // Assuming the names are semicolon separated
 
-        foreach (var csFile in csFilesList)
-        {
-            var fullPath = Path.Combine(projectPath, csFile);
-            var code = File.ReadAllText(fullPath);
-            var syntaxTree = CSharpSyntaxTree.ParseText(code);
-            var root = syntaxTree.GetRoot();
-            var relativeFilePath = fullPath.Substring(projectPath.Length).TrimStart('\\', '/');
+        //foreach (var csFile in csFilesList)
+        //{
+        //    var fullPath = Path.Combine(projectPath, csFile);
+        //    var code = File.ReadAllText(fullPath);
+        //    var syntaxTree = CSharpSyntaxTree.ParseText(code);
+        //    var root = syntaxTree.GetRoot();
+        //    var relativeFilePath = fullPath.Substring(projectPath.Length).TrimStart('\\', '/');
 
-            foreach (var classDeclaration in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
-            {
-                var className = classDeclaration.Identifier.ValueText;
-                if (!IsPascalCase(className))
-                {
-                    nonPascalNames.Add($"Pascal Rule{Environment.NewLine}" +
-                        $"class name: {className}{Environment.NewLine}" +
-                        $"File name: {relativeFilePath}{Environment.NewLine}" +
-                        $"line number: {classDeclaration.GetLocation().GetLineSpan().StartLinePosition.Line + 1}{Environment.NewLine}" +
-                        $"project name: {Path.GetFileNameWithoutExtension(csFile)}{Environment.NewLine}");
-                }
+        //    foreach (var classDeclaration in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
+        //    {
+        //        var className = classDeclaration.Identifier.ValueText;
+        //        if (!IsPascalCase(className))
+        //        {
+        //            nonPascalNames.Add($"Pascal Rule{Environment.NewLine}" +
+        //                $"class name: {className}{Environment.NewLine}" +
+        //                $"File name: {relativeFilePath}{Environment.NewLine}" +
+        //                $"line number: {classDeclaration.GetLocation().GetLineSpan().StartLinePosition.Line + 1}{Environment.NewLine}" +
+        //                $"project name: {Path.GetFileNameWithoutExtension(csFile)}{Environment.NewLine}");
+        //        }
 
-                foreach (var method in classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>())
-                {
-                    var methodName = method.Identifier.ValueText;
-                    if (!IsPascalCase(methodName))
-                    {
-                        nonPascalNames.Add($"Pascal Rule{Environment.NewLine}" +
-                            $"method name: {methodName}{Environment.NewLine}" +
-                            $"class name: {className}{Environment.NewLine}" +
-                            $"File name: {relativeFilePath}{Environment.NewLine}" +
-                            $"line number: {method.GetLocation().GetLineSpan().StartLinePosition.Line + 1}{Environment.NewLine}" +
-                            $"project name: {Path.GetFileNameWithoutExtension(csFile)}{Environment.NewLine}");
-                    }
-                }
-            }
-        }
+        //        foreach (var method in classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>())
+        //        {
+        //            var methodName = method.Identifier.ValueText;
+        //            if (!IsPascalCase(methodName))
+        //            {
+        //                nonPascalNames.Add($"Pascal Rule{Environment.NewLine}" +
+        //                    $"method name: {methodName}{Environment.NewLine}" +
+        //                    $"class name: {className}{Environment.NewLine}" +
+        //                    $"File name: {relativeFilePath}{Environment.NewLine}" +
+        //                    $"line number: {method.GetLocation().GetLineSpan().StartLinePosition.Line + 1}{Environment.NewLine}" +
+        //                    $"project name: {Path.GetFileNameWithoutExtension(csFile)}{Environment.NewLine}");
+        //            }
+        //        }
+        //    }
+        //}
 
-        if (nonPascalNames.Any())
-        {
-            var reportContent = string.Join(Environment.NewLine, nonPascalNames);
+        //if (nonPascalNames.Any())
+        //{
+        //    var reportContent = string.Join(Environment.NewLine, nonPascalNames);
 
-            File.WriteAllText(reportFilePath, reportContent);
-            Console.WriteLine("Non-PascalCase names report saved.");
-        }
-        else
-        {
-            Console.WriteLine("All names follow PascalCase.");
-        }
+        //    File.WriteAllText(reportFilePath, reportContent);
+        //    Console.WriteLine("Non-PascalCase names report saved.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("All names follow PascalCase.");
+        //}
     }
 
     static bool IsPascalCase(string s)
-    {
+    {s
         return !string.IsNullOrEmpty(s) && char.IsUpper(s[0]);
     }
 }
