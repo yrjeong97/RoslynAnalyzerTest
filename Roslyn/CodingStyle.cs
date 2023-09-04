f7c50d46d4de5452f48e594935e98f66881a88fc
yrjeong97 <yrjeong@ati2000.co.kr>
2023-09-04 13:09:08 +0900

    test string rule

--- Roslyn/CodingStyle.cs
+++ Roslyn/CodingStyle.cs
@@ -26,7 +26,7 @@ namespace Roslyn
 
         public List<string> AnalyzeCodingStyle()
         {
-            foreach(var csFile in csFilesList)
+            foreach (var csFile in csFilesList)
             {
                 var fullPath = Path.Combine(projectPath, csFile);
                 var code = File.ReadAllText(fullPath);
@@ -41,70 +41,66 @@ namespace Roslyn
 
         void AnalyzeStringConcatenation(SyntaxNode root, string csFile)
         {
-            List<string> violations = new List<string>();
+            //foreach (var declaration in root.DescendantNodes().OfType<VariableDeclarationSyntax>())
+            //{
+            //    var variableType = declaration.Type as PredefinedTypeSyntax;
+            //    if (variableType != null && variableType.Keyword.Text == "string")
+            //    {
+            //        var variableName = declaration.Variables.First().Identifier.Text;
+            //        var assignmentExpression = declaration.Parent.DescendantNodes().OfType<BinaryExpressionSyntax>().FirstOrDefault();
 
-            var binaryExpressions = root.DescendantNodes().OfType<BinaryExpressionSyntax>();
-            foreach (var binaryExpression in binaryExpressions)
+            //        if (assignmentExpression != null && assignmentExpression.Kind() == SyntaxKind.AddExpression)
+            //        {
+            //            int lineNum = assignmentExpression.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
+            //            noneCodingStlye.Add(WriteNamingRuleReport.WriteStringConcatenationIssue(csFile, lineNum));
+            //        }
+            //    }
+            //}
+
+            var uninitializedVariables = new HashSet<string>();
+
+            foreach (var declaration in root.DescendantNodes().OfType<VariableDeclarationSyntax>())
             {
-                if (binaryExpression.Left is IdentifierNameSyntax leftIdentifier && binaryExpression.Right is IdentifierNameSyntax rightIdentifier)
+                var variableType = declaration.Type as PredefinedTypeSyntax;
+                if (variableType != null && variableType.Keyword.Text == "string")
                 {
-                    if (leftIdentifier.ToString() == "string" || rightIdentifier.ToString() == "string")
+                    foreach (var variable in declaration.Variables)
                     {
-                        int lineNum = binaryExpression.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
-                        noneCodingStlye.Add(WriteNamingRuleReport.WriteStringConcatenationIssue(csFile, lineNum));
+                        if (variable.Initializer == null)
+                        {
+                            uninitializedVariables.Add(variable.Identifier.Text);
+                        }
                     }
                 }
             }
+
+            foreach (var binaryExpression in root.DescendantNodes().OfType<BinaryExpressionSyntax>())
+            {
+                var leftIdentifier = binaryExpression.Left as IdentifierNameSyntax;
+                var rightIdentifier = binaryExpression.Right as IdentifierNameSyntax;
+
+                if (leftIdentifier != null && uninitializedVariables.Contains(leftIdentifier.Identifier.Text))
+                {
+                    int lineNum = binaryExpression.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
+                    noneCodingStlye.Add(WriteNamingRuleReport.WriteStringConcatenationIssue(csFile, lineNum));
+                }
+
+                if (rightIdentifier != null && uninitializedVariables.Contains(rightIdentifier.Identifier.Text))
+                {
+                    int lineNum = binaryExpression.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
+                    noneCodingStlye.Add(WriteNamingRuleReport.WriteStringConcatenationIssue(csFile, lineNum));
+                }
+            }
         }
-            
-       
 
-        //void AnalyzeStringConcatenation(SyntaxNode root, string csFile)
-        //{
-        //    var uninitializedVariables = new HashSet<string>();
-
-        //    foreach (var declaration in root.DescendantNodes().OfType<VariableDeclarationSyntax>())
-        //    {
-        //        var variableType = declaration.Type as PredefinedTypeSyntax;
-        //        if (variableType != null && variableType.Keyword.Text == "string")
-        //        {
-        //            foreach (var variable in declaration.Variables)
-        //            {
-        //                if (variable.Initializer == null)
-        //                {
-        //                    uninitializedVariables.Add(variable.Identifier.Text);
-        //                }
-        //            }
-        //        }
-        //    }
-
-        //    foreach (var binaryExpression in root.DescendantNodes().OfType<BinaryExpressionSyntax>())
-        //    {
-        //        var leftIdentifier = binaryExpression.Left as IdentifierNameSyntax;
-        //        var rightIdentifier = binaryExpression.Right as IdentifierNameSyntax;
-
-        //        if (leftIdentifier != null && uninitializedVariables.Contains(leftIdentifier.Identifier.Text))
-        //        {
-        //            int lineNum = binaryExpression.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
-        //            noneCodingStlye.Add(WriteNamingRuleReport.WriteStringConcatenationIssue(csFile, lineNum));
-        //        }
-
-        //        if (rightIdentifier != null && uninitializedVariables.Contains(rightIdentifier.Identifier.Text))
-        //        {
-        //            int lineNum = binaryExpression.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
-        //            noneCodingStlye.Add(WriteNamingRuleReport.WriteStringConcatenationIssue(csFile, lineNum));
-        //        }
-        //    }
-        //}
-
-        //bool IsStringConcatenationRule(BinaryExpressionSyntax binaryExpression)
-        //{
-        //    if (binaryExpression.Left is LiteralExpressionSyntax leftLiteral && leftLiteral.IsKind(SyntaxKind.StringLiteralExpression)
-        //                && binaryExpression.Right is LiteralExpressionSyntax rightLiteral && rightLiteral.IsKind(SyntaxKind.StringLiteralExpression))
-        //    {
-        //        return true;
-        //    }
-        //    return false;
-        //}
+        bool IsStringConcatenationRule(BinaryExpressionSyntax binaryExpression)
+        {
+            if (binaryExpression.Left is LiteralExpressionSyntax leftLiteral && leftLiteral.IsKind(SyntaxKind.StringLiteralExpression)
+                        && binaryExpression.Right is LiteralExpressionSyntax rightLiteral && rightLiteral.IsKind(SyntaxKind.StringLiteralExpression))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
